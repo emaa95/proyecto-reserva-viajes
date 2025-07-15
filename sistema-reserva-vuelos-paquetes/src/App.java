bdf9cf8d0c893ca00a8662613a2058d052abfb96
emaa95 <chiletti.ema@gmail.com>
2025-07-15 11:07:33 -0300

    Se agregaron datos de prueba

--- sistema-reserva-vuelos-paquetes/src/App.java
+++ sistema-reserva-vuelos-paquetes/src/App.java
@@ -1,7 +1,9 @@
+import datos.Datos;
 import menus.Menu;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        Datos.precargarVendedores();
         Menu.mostrarMenuPrincipal();
     }
 }
--- sistema-reserva-vuelos-paquetes/src/datos/Datos.java
+++ sistema-reserva-vuelos-paquetes/src/datos/Datos.java
@@ -1,5 +1,15 @@
 package datos;
 
-public class Datos {
+import clases.Vendedor;
 
+public class Datos {
+    public static void precargarVendedores() {
+        agregarVendedor(new Vendedor("Juan", "Pérez", "12345678", "V001", "Full time"));
+        agregarVendedor(new Vendedor("María", "Gómez", "87654321", "V002", "Part time"));
+        agregarVendedor(new Vendedor("Luis", "Rodríguez", "11223344", "V003", "Full time"));
+    }
+
+    private static void agregarVendedor(Vendedor v) {
+        Vendedor.agregarVendedor(v);
+    }
 }
