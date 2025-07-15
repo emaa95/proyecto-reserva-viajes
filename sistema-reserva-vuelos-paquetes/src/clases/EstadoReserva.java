0582c52f4c3f171cbe2df29d821b12b63c07ed73
emaa95 <chiletti.ema@gmail.com>
2025-07-15 11:08:33 -0300

    Se crearon clases enum

---
+++ sistema-reserva-vuelos-paquetes/src/clases/EstadoReserva.java
@@ -0,0 +1,18 @@
+package clases;
+
+public enum EstadoReserva {
+    PENDIENTE,
+    EN_PROCESO,
+    RESERVADA,
+    CONFIRMADA,
+    PAGADA,
+    EN_CURSO,
+    COMPLETADA,
+    CANCELADA,
+    RECHAZADA,
+    EXPIRADA,
+    FALLIDA,
+    REEMBOLSADA,
+    MODIFICADA,
+    SUSPENDIDA
+}
